b591844be64ad242a897cb660bd2cd547b997f20
João Vitor Pinto Andrade <joao.andrade@fitbank.com.br>
2021-08-16 12:09:33 -0300

    Send Project

---
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models.ViewModels;
+
+namespace SalesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Iam"] = "João Vitor Pinto Andrade";
+            ViewData["Message"] = "Inter at Fitbank";
+            ViewData["Email"] = "joaodbv@outlook.com";
+            ViewData["MotivationalPhrase"] = "Cow don't give milk you need gonna get!";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Double BaseSalary { get; set; }
+        public Department Department { get; set; }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales
+                .Where(sr => sr.Date >= initial && sr.Date >= final)
+                .Sum(sr => sr.Amount);
+        }
+    }
+}
